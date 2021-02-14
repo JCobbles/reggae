8adac6b06c77e2700e98980e344ef2c8bf1a025a
Jacob Moss <cob.mossy@gmail.com>
2021-02-14 20:35:49 +0000

    add initial conditions to transcriptional lfm

--- reggae/gp/variational/models/transcriptional.py
+++ reggae/gp/variational/models/transcriptional.py
@@ -4,6 +4,7 @@ import torch
 from torch.nn.parameter import Parameter
 from torch.distributions.normal import Normal
 
+from torchdiffeq import odeint
 from .model import VariationalLFM
 from reggae.utilities import softplus, LFMDataset
 
@@ -11,9 +12,48 @@ from reggae.utilities import softplus, LFMDataset
 class TranscriptionalRegulationLFM(VariationalLFM):
     def __init__(self, num_outputs, num_latents, t_inducing, dataset: LFMDataset, extra_points=2, **kwargs):
         super().__init__(num_outputs, num_latents, t_inducing, dataset, extra_points=extra_points, **kwargs)
+        self.initial_condition = Parameter(0.4+torch.rand((self.num_outputs, 1), dtype=torch.float64))
         self.decay_rate = Parameter(1 + torch.rand((self.num_outputs, 1), dtype=torch.float64))
         self.basal_rate = Parameter(torch.rand((self.num_outputs, 1), dtype=torch.float64))
         self.sensitivity = Parameter(1.5 + torch.rand((self.num_outputs, 1), dtype=torch.float64))
+
+    def forward(self, t, h, rtol=1e-4, atol=1e-6, compute_var=False):
+        """
+        t : torch.Tensor
+            Shape (num_times)
+        h : torch.Tensor the initial state of the ODE
+            Shape (num_genes, 1)
+        Returns
+        -------
+        Returns evolved h across times t.
+        Shape (num_genes, num_points).
+        """
+        self.nfe = 0
+
+        # Precompute variables
+        self.Kmm = self.rbf(self.inducing_inputs)
+        self.L = torch.cholesky(self.Kmm)
+        # self.inv_Kmm = cholesky_inverse(self.L)
+        q_cholS = torch.tril(self.q_cholS)
+        self.S = torch.matmul(q_cholS, torch.transpose(q_cholS, 1, 2))
+
+        h0 = self.initial_state(h)
+        # Integrate forward from the initial positions h.
+        h_samples = odeint(self.odefunc, h0, t, method='dopri5', rtol=rtol, atol=atol)  # (T, S, num_outputs, 1)
+
+        h_avg = torch.mean(h_samples, dim=1)
+        h_std = torch.std(h_samples, dim=1)
+
+        h_out = torch.transpose(h_avg, 0, 1)
+        h_std = torch.transpose(h_std, 0, 1)
+        bd = self.basal_rate / self.decay_rate
+        edt = torch.exp(-self.decay_rate * t)
+        h_out = bd + (self.initial_condition - bd)*edt + edt * h_out.squeeze(-1)
+        h_out = h_out.unsqueeze(-1)
+
+        if compute_var:
+            return self.decode(h_out), h_std
+        return self.decode(h_out)
 
     def odefunc(self, t, h):
         """h is of shape (num_samples, num_outputs, 1)"""
@@ -21,7 +61,7 @@ class TranscriptionalRegulationLFM(VariationalLFM):
         # if (self.nfe % 100) == 0:
         #     print(t)
 
-        decay = torch.multiply(self.decay_rate.squeeze(), h.squeeze(-1)).view(self.num_samples, -1, 1)
+        # decay = torch.multiply(self.decay_rate.squeeze(), h.squeeze(-1)).view(self.num_samples, -1, 1)
 
         q_f = self.get_latents(t.reshape(-1))
 
@@ -33,8 +73,8 @@ class TranscriptionalRegulationLFM(VariationalLFM):
             f = f[:, :, self.extra_points]  # get the midpoint
             f = torch.unsqueeze(f, 2)
 
-        return self.basal_rate + self.sensitivity * f - decay
-
+        # return self.basal_rate + self.sensitivity * f - decay
+        return torch.exp(self.decay_rate * t) * f
 
     @abstractmethod
     def G(self, f):
